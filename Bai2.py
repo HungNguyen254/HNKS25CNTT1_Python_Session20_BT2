b40f27c6814391033c3ff046d07fd655fb7fc703
HungNguyen254 <nguyenhung2540206@gmail.com>
2026-06-10 20:44:07 +0700

    Add files via upload

---
+++ Bai2.py
@@ -0,0 +1,28 @@
+#ở levi thì code chạy được nhưng sofm thì code bị lỗi vì ở levi có đầy đủ thông tin ở 3 vị trí index khác nhau còn với sofm thì chỉ có 2 thông tin ở vị trí index đầu bị thiếu thông tin dữ liệu ở vị trí index cuối
+#khi đến optimus bị lỗi ở dòng b = (m * 10) + (int(r) * 0.5) vì ở vị trí index thứ 2 thông tin dữ liệu của optimus nó lại lưu dưới dạng chuỗi là chữ nên sẽ gây ra lỗi khi thực hiện tính toán
+# dòng code sẽ in từng dòng cho thấy đang xử lý đến tuyển thủ nào và rất dễ để biết được rằng tuyển thủ nào đang gây lỗi cho chương trình
+#
+# Dữ liệu từ API: (Tên, Số trận, MMR)
+data = [
+    ("Levi", 120, 2500),      # Dữ liệu chuẩn
+    ("SofM", 150,2800),            # Lỗi API: Bị thiếu mất trường MMR (Tuple chỉ có 2 phần tử)
+    ("Optimus", 100, "N/A")   # Lỗi dữ liệu: Điểm MMR bị ghi chữ "N/A"
+]
+
+# Hàm xử lý dồn cục, không có cơ chế bẫy lỗi
+def process(ds):
+    print("--- BẢNG TÍNH THƯỞNG RP ---")
+    for p in ds:
+        print("Đang xử lý:", p)
+        t = p[0]
+        m = p[1]
+        r = p[2]  # Lấy điểm MMR
+        
+        # Tính toán tiền thưởng
+        try:
+            b = (m * 10) + (int(r) * 0.5)
+            print("Tuyển thủ", t, "nhận được", b, "RP")
+        except ValueError:
+            print(f'Tuyển thủ {t} Lỗi - Dữ liệu MMR không hợp lệ')
+# Chạy hệ thống
+process(data)
